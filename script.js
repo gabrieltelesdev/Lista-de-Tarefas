55887be63b658f1d971a15664bc6376dde3614ae
Gabriel Teles <gabrieltelesdev@hotmail.com>
2022-01-24 21:49:55 -0300

    Implementada as funções de adicionar e imprimir tarefa na tela, excluir tarefas específicas e excluir uma lista inteira utilizando JS

---
+++ script.js
@@ -0,0 +1,55 @@
+const elementoInput = document.querySelector("input");
+const elementoLista = document.querySelector("ul");
+const addBtn = document.querySelector("#add");
+const limparBtn = document.querySelector("#limpar")
+
+const tarefas = [];
+
+
+function imprimirTarefas(){
+
+    elementoLista.innerHTML = "";
+
+    for(item of tarefas){
+    const itemLista = document.createElement("li");
+    const itemTexto = document.createTextNode(item);
+
+    itemLista.appendChild(itemTexto);
+    elementoLista.appendChild(itemLista);
+
+    const itemDelete = document.createElement("a");
+    const textDelete = document.createTextNode("X");
+    const pos = tarefas.indexOf(item);
+
+    itemDelete.setAttribute("href", "#")
+    itemDelete.setAttribute("onclick", `deletaTarefa(${pos})`)
+
+    itemDelete.appendChild(textDelete);
+    itemLista.appendChild(itemDelete);
+
+    }
+}
+
+imprimirTarefas();
+
+function addTarefas(){
+    const valorTarefa = elementoInput.value;
+    tarefas.push(valorTarefa);
+    elementoInput.value = "";
+
+    imprimirTarefas();
+}
+
+addBtn.setAttribute('onclick', 'addTarefas()');
+
+function deletaTarefa(pos){
+    tarefas.splice(pos, 1);
+    imprimirTarefas();
+}
+
+function deletaLista(pos){
+    tarefas.splice(pos, tarefas.length);
+    imprimirTarefas();
+}
+
+limparBtn.setAttribute('onclick', 'deletaLista()');
